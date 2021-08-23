c21a8da7e909654c34425a2b9f8c2eb51e9730b4
lilu1982 <homenetmedia82@gmail.com>
2021-08-23 10:51:57 +0200

    Daten Logik

--- js/model.js
+++ js/model.js
@@ -1,1 +1,16 @@
 /* Model (Daten/Logik) */
+
+
+    let data = {
+                milk:{lower:0, upper:5, bev:"milch"},
+                juice:{lower:6, upper:12, bev:"saft"},
+                cola:{lower:13, upper:20, bev:"cola"},
+                wine:{lower:21, upper:130, bev:"wein"},
+                default:{bev:"tee"}
+                }
+    
+    
+    
+    let gui =   {
+                    img:{path:"./bilder/", ext:".jpg"}
+                }
