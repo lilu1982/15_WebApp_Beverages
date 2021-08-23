4e3c222c523c668a6afa87dd69f0449ea9caaa72
Peter <schmidt.multimedia@gmail.com>
2021-08-23 09:17:38 +0200

    01 / Modul: Bild aktualisieren

--- js/view_controller.js
+++ js/view_controller.js
@@ -6,7 +6,7 @@
 	Check Daten :: toDo
     Btn. Trigger :: toDo
     Business-Logic (Alter --> Getränk) :: toDo
-    Bild austauschen :: toDo
+    Bild austauschen :: check!
 */
 
 // Modul Ablaufsteuerung | Test:
@@ -33,7 +33,13 @@
 
 
 // Modul: Bild aktualisieren | Test:
-
+//ausgabe(updateImg("cola"));
+//ausgabe(updateImg("milch"));
+function updateImg(imgName) {
+    let img = document.getElementById("bevImg");
+    img.src = "./bilder/" + imgName + ".jpg";
+    return imgName; // monitoring
+}
 
 //Modul: Konsolenausgabe --> Test:
 function ausgabe(outputStr) {
