47a1093fa1bb385f9a60c74672a890794570062d
lilu1982 <homenetmedia82@gmail.com>
2021-08-23 10:51:37 +0200

    abflaufsteuerung

--- js/view_controller.js
+++ js/view_controller.js
@@ -1,18 +1,17 @@
-
 /* View- Controller */
 
 /* Der Plan
     Einlesen Daten von Webseite :: toDo
 	Check Daten :: toDo
     Btn. Trigger :: toDo
-    Business-Logic (Alter --> Getränk) :: toDo
+    Business-Logic (Alter --> Getränk) :: check
     Bild austauschen :: check!
 */
 
 // Modul Ablaufsteuerung | Test:
-controller()
+//  controller()
 function controller() {
-    ausgabe(updateImg(checkAge()));
+    ausgabe(updateImg(checkAge(getInput())));
 }
 
 
@@ -26,26 +25,47 @@ function controller() {
 // Event-Dispatcher
 
 
+
 // Modul: Check auf korrekte Eingaben ...
 
 
 // Modul: Eingabe | Test:
-
-
-//Modul: Business-Logic (Mapping) | Test:
-// ausgabe(checkAge());
-function checkAge() {
-    return "cola";
+//ausgabe(getInput())
+function getInput() {
+    return 2;
+    
 }
 
 
+//Modul: Business-Logic (Mapping) | Test:
+// ausgabe(checkAge(2));
+// ausgabe(checkAge(7));
+// ausgabe(checkAge(13));
+// ausgabe(checkAge(18));
+// ausgabe(checkAge(99));
+// ausgabe(checkAge(135));
+function checkAge(age) {
+    switch (true) 
+    {
+        case (age >= 0) && (age <= 5):
+            return "milch";
+        case (age >= 6) && (age <= 12):
+            return "saft";
+        case (age >= 13) && (age <= 17):
+            return "cola";
+        case (age >= 18) && (age <= 130):
+            return "wein";
+        default:
+            return "tee";
+    }
+}
 
 // Modul: Bild aktualisieren | Test:
-//ausgabe(updateImg("cola"));
+// ausgabe(updateImg("cola"));
 //ausgabe(updateImg("milch"));
 function updateImg(imgName) {
     let img = document.getElementById("bevImg");
-    img.src = "./bilder/" + imgName + ".jpg";
+    img.src = gui.img.path + imgName + gui.img.ext;
     return imgName; // monitoring
 }
 
