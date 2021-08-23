766d3a0b9e7720ef63b1e4b822e4408acd3e1b06
lilu1982 <homenetmedia82@gmail.com>
2021-08-23 12:00:39 +0200

    deutsche grenzen

--- js/model.js
+++ js/model.js
@@ -4,8 +4,8 @@
     let data = {
                 milk:{lower:0, upper:5, bev:"milch"},
                 juice:{lower:6, upper:12, bev:"saft"},
-                cola:{lower:13, upper:20, bev:"cola"},
-                wine:{lower:21, upper:130, bev:"wein"},
+                cola:{lower:13, upper:17, bev:"cola"},
+                wine:{lower:18, upper:130, bev:"wein"},
                 default:{bev:"tee"}
                 }  
         
