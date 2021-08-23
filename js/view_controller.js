2b2e8062fa51e87578e797628ab4a26aef8f35a9
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-08-23 07:58:41 +0200

    Add files via upload

---
+++ js/view_controller.js
@@ -0,0 +1,41 @@
+
+/* View- Controller */
+
+/* Der Plan
+    Einlesen Daten von Webseite :: toDo
+	Check Daten :: toDo
+    Btn. Trigger :: toDo
+    Business-Logic (Alter --> Getränk) :: toDo
+    Bild austauschen :: toDo
+*/
+
+// Modul Ablaufsteuerung | Test:
+//controller()
+
+
+
+// Trigger - Btn 
+
+
+// Trigger - Input
+
+
+// Event-Dispatcher
+
+
+// Modul: Check auf korrekte Eingaben ...
+
+
+// Modul: Eingabe | Test:
+
+
+//Modul: Business-Logic (Mapping) | Test:
+
+
+// Modul: Bild aktualisieren | Test:
+
+
+//Modul: Konsolenausgabe --> Test:
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
